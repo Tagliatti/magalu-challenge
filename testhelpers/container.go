9cead278ec656858fdc18eb2e179c53222a40afa
Filipe Tagliatti <filipetagliatti@gmail.com>
2025-04-09 20:23:43 -0300

    test: adicionado pacote de integração com testcontainers
    O pacote tem algumas funções para facilitar a criação de um container
    postgres para os testes de integração e limpesa do banco de dados de
    teste.

---
+++ testhelpers/container.go
@@ -0,0 +1,74 @@
+package testhelpers
+
+import (
+	"context"
+	"database/sql"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/postgres"
+	"github.com/testcontainers/testcontainers-go/wait"
+	"path/filepath"
+	"time"
+)
+
+type PostgresContainer struct {
+	*postgres.PostgresContainer
+	ConnectionString string
+}
+
+func NewPostgresContainer(ctx context.Context) (*PostgresContainer, error) {
+	files, err := migrationFiles()
+	pgContainer, err := postgres.Run(ctx, "postgres:17-bullseye",
+		postgres.WithInitScripts(files...),
+		postgres.WithDatabase("test"),
+		postgres.WithUsername("postgres"),
+		postgres.WithPassword("postgres"),
+		testcontainers.WithWaitStrategy(
+			wait.ForLog("database system is ready to accept connections").
+				WithOccurrence(2).
+				WithStartupTimeout(5*time.Second)),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	connectionString, err := pgContainer.ConnectionString(ctx, "sslmode=disable")
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &PostgresContainer{
+		PostgresContainer: pgContainer,
+		ConnectionString:  connectionString,
+	}, nil
+}
+
+func TruncateAllTables(ctx context.Context, db *sql.DB) error {
+	_, err := db.ExecContext(ctx, `
+		DO $$ DECLARE
+			r RECORD;
+		BEGIN
+			FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema()) LOOP
+				EXECUTE 'TRUNCATE TABLE ' || quote_ident(r.tablename) || '';
+			END LOOP;
+		END $$;
+	`)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func migrationFiles() ([]string, error) {
+	basePath := filepath.Join("..", "migration")
+	files, err := filepath.Glob(filepath.Join(basePath, "*.sql"))
+
+	if err != nil {
+		return nil, err
+	}
+
+	return files, nil
+}
