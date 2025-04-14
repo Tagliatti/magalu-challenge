72c93d571583d92b624d5fa3f0338209103a773f
Filipe Tagliatti <filipetagliatti@gmail.com>
2025-04-14 18:38:45 -0300

    style: movido main da api para uma pasta cmd propria

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"context"
 	"github.com/Tagliatti/magalu-challenge/database"
 	"github.com/Tagliatti/magalu-challenge/health"
 	"github.com/Tagliatti/magalu-challenge/notifications"
 	"github.com/Tagliatti/magalu-challenge/notifications/handler"
 	"log"
+	"net"
 	"net/http"
 )
 
 func main() {
-	db, err := database.Connect()
+	ctx := context.Background()
+	db, err := database.Connect(ctx, 10, 100)
 
 	if err != nil {
 		log.Fatal(err)
@@ -24,12 +27,20 @@ func main() {
 	statusNotification := handler.NewStatusHandler(notificationStorage)
 	deleteNotification := handler.NewDeleteHandler(notificationStorage)
 
-	server := http.NewServeMux()
-	server.HandleFunc("POST /notifications", createNotification.Handler)
-	server.HandleFunc("GET /notifications/{id}/status", statusNotification.Handler)
-	server.HandleFunc("DELETE /notifications/{id}", deleteNotification.Handler)
-	server.HandleFunc("/", healthy.Handler)
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /notifications", createNotification.Handler)
+	mux.HandleFunc("GET /notifications/{id}/status", statusNotification.Handler)
+	mux.HandleFunc("DELETE /notifications/{id}", deleteNotification.Handler)
+	mux.HandleFunc("/", healthy.Handler)
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+		BaseContext: func(_ net.Listener) context.Context {
+			return ctx
+		},
+	}
 
 	log.Println("Servidor iniciado na porta 8080...")
-	log.Fatal(http.ListenAndServe(":8080", server))
+	log.Fatal(server.ListenAndServe())
 }
